e4ef5e04c197f1facc706cb215df371ba3a4cc61
Aisha <111194434+Aisha-Saad@users.noreply.github.com>
2025-05-22 18:47:17 +0300

    header-section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+
+
+
 
 const Header = () => {
+  const [navOpen,setNavopen]=useState(true)
   return (
     <header className='fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0'>
-        <div className='max-w-screen-2xl w-full mx-auto px-4'>
+        <div className='max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6 md:grid md:grid-cols-[1fr,3fr,1fr]'>
             <h1>
                 <a href="/" className='logo'>
-                <img src= "" alt="" width={40}  height={40}/>
+                <img src= "/images/logo.svg" alt="" width={40}  height={40}/>
                 </a>
             </h1>
+            <div className='relative md:justify-self-center'>
+              <button className='menu-btn md:hidden ' onClick={null}>
+                <span className='material-symbols-rounded'></span>
+              </button>
+              <Navbar navOpen={navOpen}/>
+            </div>
+
+            <a href="#contect" className='btn btn-secondary max-md:hidden md:justify-self-center'>Contact Me</a>
 
         </div>
     </header>
